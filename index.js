a4929838c4d543ff6b40410aa0893aa11e889e1a
Oxana Chizitcaia <creativesouloxana@gmail.com>
2024-05-28 15:25:25 +0200

    Added solution

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-'use strict';
+const formElm = document.querySelector('.controls');
 
-console.log('funguju!');
+formElm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const timeInput = Number(document.querySelector('.time-input').value);
+  let time = timeInput * 60;
+
+  const startCountdown = setInterval(() => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    document.querySelector('.alarm__minutes').textContent = String(
+      minutes,
+    ).padStart(2, '0');
+    document.querySelector('.alarm__seconds').textContent = String(
+      seconds,
+    ).padStart(2, '0');
+    const alarmElm = document.querySelector('.alarm');
+
+    time -= 1;
+
+    if (time === -1) {
+      clearInterval(startCountdown);
+      document.querySelector('audio').play();
+      alarmElm.classList.add('alarm--ring');
+    }
+  }, 1000);
+});
